d9c3c92e8ae5d49148523f2d1ce10044b7714270
cpjohnso <43118620+cpjohnso@users.noreply.github.com>
2018-11-29 14:08:10 -0500

    Add files via upload

---
+++ 2018_Fall_Assignment4_final/IEXTrading/Models/Headline.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace IEXTrading.Models
+{
+    public class Headline
+    {
+        public string datetime { get; set; }
+        public string headline { get; set; }
+        public string source { get; set; }
+        public string summary { get; set; }
+        [Key]
+        public string url { get; set; }
+        public string related { get; set; }
+    }
+}
